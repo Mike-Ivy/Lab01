cb238c00180550e420897a515e84ac3becb1af78
Michael Aguirre <112357832+Mike-Ivy@users.noreply.github.com>
2022-08-29 02:37:26 +0000

    Created the drawTriangle function.

--- LAB_ JavaScript loops/script.js
+++ LAB_ JavaScript loops/script.js
@@ -1,5 +1,14 @@
 function drawTriangle(triangleSize) { 
-
   
+  let n = triangleSize;
+  let string = "";
+  
+  for (let i = 1; i <= n; i++) {
+    for (let j = 0; j < i; j++) {
+      string += "*";
+    }
+    string += "\n";
+  }
    
 }
+drawTriangle(4)
