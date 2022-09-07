951371005606686cac0ef96b96feaaa390d5480d
Michael Aguirre <112357832+Mike-Ivy@users.noreply.github.com>
2022-09-07 19:30:06 +0000

    new push 2

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function drawTriangle(n) {
   let output = "";
   for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= i; j++) {
+    for (let j = 0; j < i; j++) {
       output += "*";
     }
     output += "\n";
   }
   console.log(output);
 }
-drawTriangle();
+drawTriangle(4);
