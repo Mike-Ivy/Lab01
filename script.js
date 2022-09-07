0ea01e92c781318fc68e9d569a3e64f4330e1a30
Michael Aguirre <112357832+Mike-Ivy@users.noreply.github.com>
2022-09-07 17:45:16 +0000

    Fixed drawTriangle function.

---
+++ script.js
@@ -0,0 +1,13 @@
+function drawTriangle(n) {
+  let output = "";
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      output += "*";
+    }
+    output += "\n";
+  }
+  console.log(output);
+}
+
+drawTriangle(4);
+
